37506f96400cd5a3820a6f7a5bc099a10414dccb
Hardik <hardikralhan66@gmail.com>
2022-02-04 23:05:43 +0530

    Change in name

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Hardik";
+ReactDOM.render(<h1>Hello {name} !</h1>, document.getElementById("root"));
